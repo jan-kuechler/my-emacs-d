ac9b266214029d7f09666325e58f20272891788f
Kuchen <kuchen_@gmx.de>
2010-07-03 13:41:13 +0200

    Disable toolbar

--- init.el
+++ init.el
@@ -38,6 +38,9 @@
 (global-set-key "\C-z" 'undo)
 
 
+;; Disable toolbar
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+
 ;; Disable startup screen
 (custom-set-variables
  '(inhibit-startup-screen t))
