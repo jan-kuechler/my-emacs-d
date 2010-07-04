bc6ba12ea8513b512d216dbdec97110d63f2ab79
Kuchen <kuchen_@gmx.de>
2010-07-04 20:04:54 +0200

    Added site-lisp directory

--- init.el
+++ init.el
@@ -2,6 +2,9 @@
 
 (add-to-list 'load-path "~/.emacs.d/")
 (add-to-list 'load-path "~/.emacs.d/lisp")
+(add-to-list 'load-path "~/.emacs.d/site-lisp")
+
+(load-file "libs.el")
 
 ;; lua-mode
 (setq auto-mode-alist 
@@ -31,12 +34,24 @@
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (ac-config-default)
 
+;; window hopping
+(defun other-window-backward (n)
+  "Select n-th prev. window."
+  (interactive "p")
+  (other-window (- n)))
+
 ;; keybindings
 
 (global-set-key "\C-w" 'backward-kill-word)
 (global-set-key "\C-q" 'kill-region)
 (global-set-key "\C-z" 'undo)
 
+(global-set-key [(M-down)] 'other-window)
+(global-set-key [(M-up)] 'other-window-backward)
+
+(global-set-key [f5] 'call-last-kbd-macro)
+
+(defalias 'qr 'query-replace)
 
 ;; Disable toolbar
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
