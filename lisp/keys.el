d36e7d980f2f37a0a3f5ec823fd29a33565f9ec4
Kuchen <kuchen_@gmx.de>
2010-07-04 20:27:16 +0200

    Keybindings moved to an own file

---
+++ lisp/keys.el
@@ -0,0 +1,10 @@
+;; My keybindings
+
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-q" 'kill-region)
+(global-set-key "\C-z" 'undo)
+
+(global-set-key [(M-down)] 'other-window)
+(global-set-key [(M-up)] 'other-window-backward)
+
+(global-set-key [f5] 'call-last-kbd-macro)
